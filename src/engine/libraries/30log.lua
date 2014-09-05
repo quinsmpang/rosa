f0cb6614bb02c940ad8689d20a3039572c818f8f
Asmageddon <Asmageddon@gmail.com>
2014-09-05 22:36:41 +0200

    Updated 30log to my own branch with support for properties

--- src/engine/libraries/30log.lua
+++ src/engine/libraries/30log.lua
@@ -1,4 +1,5 @@
 --Copyright (c) 2012 - 2014 Roland Yonaba
+--Copyright (c) 2014 Llamageddon <asmageddon@gmail.com>
 --
 --Permission is hereby granted, free of charge, to any person obtaining a
 --copy of this software and associated documentation files (the
@@ -33,19 +34,19 @@ local class
 local function deep_copy(t, dest, aType)
   local t = t or {}
   local r = dest or {}
-  
+
   for k,v in pairs(t) do
-    if aType and type(v)==aType then 
-      r[k] = v 
+    if aType and type(v)==aType then
+      r[k] = v
     elseif not aType then
-      if type(v) == 'table' and k ~= "__index" then 
-        r[k] = deep_copy(v) 
+      if type(v) == 'table' and k ~= "__index" then
+        r[k] = deep_copy(v)
       else
         r[k] = v
       end
     end
   end
-  
+
   return r
 end
 
@@ -54,7 +55,7 @@ local function instantiate(self,...)
   local instance = deep_copy(self)
   _instances[instance] = tostring(instance)
   setmetatable(instance,self)
-  
+
   if self.__init then
     if type(self.__init) == 'table' then
       deep_copy(self.__init, instance)
@@ -62,7 +63,7 @@ local function instantiate(self,...)
       self.__init(instance, ...)
     end
   end
-  
+
   return instance
 end
 
@@ -70,7 +71,7 @@ local function extends(self,extra_params)
   local heir = {}
   _classes[heir] = tostring(heir)
   deep_copy(extra_params, deep_copy(self, heir))
-  heir.__index = heir
+  heir.__index = heir.__index or heir
   heir.super = self
   return setmetatable(heir,self)
 end
@@ -79,19 +80,19 @@ baseMt = {
   __call = function (self,...)
     return self:new(...)
   end,
-  
+
   __tostring = function(self,...)
     if _instances[self] then
-      return 
+      return
         ('object(of %s):<%s>')
           :format((rawget(getmetatable(self),'__name') or '?'), _instances[self])
     end
-    
+
     return
-      _classes[self] and 
+      _classes[self] and
       ('class(%s):<%s>')
-        :format((rawget(self,'__name') or '?'),_classes[self]) or 
-      self      
+        :format((rawget(self,'__name') or '?'),_classes[self]) or
+      self
   end
 }
 
@@ -102,16 +103,49 @@ class = function(attr)
     assert(_classes[self], 'Mixins can only be used on classes.')
     return deep_copy(include, self, 'function')
   end
-  
+
+  c.property = function(self, name, getter, setter)
+    assert(_classes[self], 'Properties can only be defined for classes')
+    if not(self.__getters or self.__setters) then
+      self.__getters = {}
+      self.__setters = {}
+
+      self.__index = function(self, index)
+        local getter = self.__getters[index]
+        if getter == false then error("The " .. index .. " property is not readable.") end
+        if getter then return rawget(self, getter)(self) else return rawget(self, index) end
+      end
+
+      self.__newindex = function(self, index, value)
+        local setter = self.__setters[index]
+        if setter == false then error("The " .. index .. " property is not writeable.") end
+        if setter then rawget(self, setter)(self, value) else rawset(self, index, value) end
+      end
+    end
+
+    if type(getter) == "function" then
+        rawset(self, "__get_" .. name, getter)
+        getter = "__get_" .. name
+    end
+
+    if type(setter) == "function" then
+        rawset(self, "__set_" .. name, setter)
+        setter = "__set_" .. name
+    end
+
+    self.__getters[name] = getter or false
+    self.__setters[name] = setter or false
+  end
+
   c.new = instantiate
   c.extends = extends
   c.__index = c
   c.__call = baseMt.__call
   c.__tostring = baseMt.__tostring
-  
+
   c.is = function(self, kind)
     local super
-    while true do 
+    while true do
       super = getmetatable(super or self)
       if super == kind or super == nil then break end
     end
